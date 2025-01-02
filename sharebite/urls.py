3dc295246a535a391fc80d18f1f37cacb2644736
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2025-01-02 17:17:43 +0100

    feat: added the url patha

--- sharebite/urls.py
+++ sharebite/urls.py
@@ -1,2 +1,12 @@
 from django.urls import path
-from .views import RegisterView, LoginView, DonationDetailView, DonationListView, Proof
+from .views import RegisterView, LoginView, DonationDetailView, DonationListView, ProofUploadView, DropOffSiteView
+
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('donations/', DonationListView.as_view(), name='donation-list'),
+    path('donations/<int:pk>/', DonationDetailView.as_view(), name='donation-detail'),
+    path('donations/<int:donation_id>/proof/', ProofUploadView.as_view(), name='proof-upload'),
+    path('dropoff-sites/', DropOffSiteView.as_view(), name='dropoff-sites'),
+]
